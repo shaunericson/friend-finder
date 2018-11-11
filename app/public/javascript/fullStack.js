2baa03f39aacffdc6611bf8ffc99144937fb77e7
Shaun Ericson <shaun.ericson@outlook.com>
2018-11-10 18:51:12 -0700

    update

--- app/public/javascript/fullStack.js
+++ app/public/javascript/fullStack.js
@@ -3,9 +3,27 @@
 $(document).ready(function () {
 
     function getFriendMatch() {
+
+        $("#friendMatch").empty();
+
         $.get("/api/friends", function (result) {
-            
-        console.log(result);
+
+            console.log(result);
+            console.log(result.name);
+            // console.log(result.length)
+            // for (x = 0; x < result.length; x++) {
+                // console.log(result[x].customerName);
+                var p1 = $("<p>");
+                p1.text("Best Friend Match: " + result.name);
+                var p3 = $("<p>");
+                p3.text("Answer 1: " + result.scores[0]);
+                var p4 = $("<p>");
+                p4.text("Answer 2: " + result.scores[1]);
+                $("#friendMatch").append(p1);
+                // $("#rezos").append(p2);
+                $("#friendMatch").append(p3);
+                $("#friendMatch").append(p4);
+            // }
 
         })
     }
